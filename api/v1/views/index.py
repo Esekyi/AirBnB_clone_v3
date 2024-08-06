55591422be5e615a0c40e1c8bc8e1d257c1bfc32
[esekyi] <[sskert10@gmail.com]>
2024-08-06 07:25:39 +0000

    4. Some stats?

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Returning the status of the api"""
 from api.v1.views import app_views
+from models import storage
 from flask import jsonify
 
 
@@ -8,3 +9,22 @@ from flask import jsonify
 def index():
     """Returning status of api"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route("/stats")
+def stats():
+    """
+    Retrieves the number of each object by type
+
+    Returns:
+        A JSON dictionary with the count of each object type.
+    """
+    obj_count = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+    }
+    return jsonify(obj_count)
