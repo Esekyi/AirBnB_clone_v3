22c406d10abf25860764a5fcec7cfedf909d2049
[esekyi] <[sskert10@gmail.com]>
2024-08-06 09:30:58 +0000

    6. State

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -2,14 +2,37 @@
 """States objects to JSON"""
 from api.v1.views import app_views
 from models import storage
-from flask import jsonify
+from models.state import State
+from flask import jsonify, abort
 
 
-@app_views.route("/states", methods=["GET"])
+@app_views.route("/states", methods=["GET"], strict_slashes=False)
 def states():
     """Retrieve all list of items in State object"""
-    states = storage.all("State").values()
+    states = storage.all(State).values()
     list_all_states = []
     for state in states:
         list_all_states.append(state.to_dict())
     return jsonify(list_all_states)
+
+
+@app_views.route("/states/<state_id>", methods=["GET"], strict_slashes=False)
+def get_state(state_id):
+    """Retrieve only one item in State object"""
+    state = storage.get(State, state_id)
+
+    if not state:
+        abort(404)
+
+    return jsonify(state.to_dict())
+
+
+@app_views.route("/states/<state_id>", methods=["DELETE"], strict_slashes=False)
+def del_state(state_id):
+    """Deletes a states with the <state_id>"""
+    state = storage.get(State, state_id)
+    if not state:
+        abort(404)
+    storage.delete(state)
+    storage.save()
+    return jsonify({}), 200
